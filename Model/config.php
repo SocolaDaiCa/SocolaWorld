b0a1d7fb11adb2584775ada99af27085cd2191c7
Socola Đại Ca <tokentien@gmail.com>
2017-12-05 18:47:37 +0700

    asdas

--- Model/config.php
+++ Model/config.php
@@ -2,4 +2,5 @@
 	define('CLIENT_ID','425249171186475');
 	define('REDIRECT_URI', $serverPath = $_SERVER["REQUEST_SCHEME"] . "://". $_SERVER["HTTP_HOST"] . "/return.php");
 	define('CLIENT_SECRET','1723fe0ec79cd0cf142c93b9010ff5d8');
+
 ?>
